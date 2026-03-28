84f9cb7732ecdce83a2a8193ad36c9270355b08e
Angel Forneiro <nforneirorosas@gmail.com>
2026-03-27 21:44:14 -0300

    Profile page

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const login = document.getElementById("login");
 login.addEventListener("click", function () {
   window.location.href = "login.html";
 });
+
+const profileBtn = document.getElementById("profile-btn");
+const slides = document.querySelector(".slides");
+const totalSlides = document.querySelectorAll(".slide").length;
+
+let currIndex = 0;
+
+profileBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  currIndex++;
+
+  if (currIndex >= totalSlides) {
+    currIndex = 0;
+  }
+
+  const desplazamiento = currentIndex * -100;
+
+  slides.style.transform = `translateX(${desplazamiento}%)`;
+});

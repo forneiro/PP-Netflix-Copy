f7d699e1613050b19fcf1fadc43d0f0f92b7d3e0
Angel Forneiro <nforneirorosas@gmail.com>
2026-03-29 15:34:03 -0300

    Structure project

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const emailInput = document.getElementById("email");
+const submitLogin = document.getElementById("submit-login");
+
+submitLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("hola");
+});
--- login.js
+++
@@ -1,1 +0,0 @@
-const emailInput = document.getElementById("email");

d6b69610ac14da72cfa735b3aafc86d53dd49744
Angel Forneiro <nforneirorosas@gmail.com>
2026-03-29 02:17:41 -0300

    Improving Carrousel index

--- index.js
+++ index.js
@@ -2,10 +2,47 @@ const login = document.getElementById("login");
 
 const trendCarrousel = document.querySelector(".trend__carrousel");
 const trendContainer = document.querySelector(".trend__container");
-const trendPrev = document.getElementById(".trend-prev");
-const trendNext = document.getElementById(".trend-next");
+const prevBtn = document.getElementById("trend-prev");
+const nextBtn = document.getElementById("trend-next");
 
 // Redirect to login page
 login.addEventListener("click", function () {
   window.location.href = "login.html";
+});
+
+let scrollAmount = 0;
+
+nextBtn.addEventListener("click", () => {
+  const containerWidth =
+    document.querySelector(".trend__container").offsetWidth;
+  const maxScroll = trendContainer.scrollWidth - containerWidth;
+
+  scrollAmount += containerWidth;
+
+  if (scrollAmount > maxScroll) scrollAmount = maxScroll;
+
+  if (scrollAmount > 1) {
+    prevBtn.classList.remove("hide");
+  } else {
+    prevBtn.classList.add("hide");
+  }
+
+  trendContainer.style.transform = `translateX(-${scrollAmount}px)`;
+});
+
+prevBtn.addEventListener("click", () => {
+  const containerWidth =
+    document.querySelector(".trend__container").offsetWidth;
+
+  scrollAmount -= containerWidth;
+
+  if (scrollAmount < 0) scrollAmount = 0;
+
+  if (scrollAmount > 1) {
+    prevBtn.classList.remove("hide");
+  } else {
+    prevBtn.classList.add("hide");
+  }
+
+  trendContainer.style.transform = `translateX(-${scrollAmount}px)`;
 });

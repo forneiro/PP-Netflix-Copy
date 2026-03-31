80a00feda8ac895189bbf53a0713120dedfcacd9
Angel Forneiro <nforneirorosas@gmail.com>
2026-03-30 23:10:50 -0300

    Add perfil

--- js/login.js
+++ js/login.js
@@ -1,7 +1,38 @@
-const emailInput = document.getElementById("email");
 const submitLogin = document.getElementById("submit-login");
+const loginErrorMessage = document.getElementById("login-error-message");
+const emailInput = document.getElementById("email");
+
+const isValidEmail = function (email) {
+  return email.includes("@") && email.length > 3 && email.includes(".");
+};
+
+const addErrorMessage = function () {
+  loginErrorMessage.classList.remove("hide");
+  loginErrorMessage.classList.add("loginErrorMessage");
+  emailInput.style.border = "1px solid var(--error)";
+};
 
 submitLogin.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("hola");
+
+  const emailInputValue = document.getElementById("email").value.trim();
+
+  if (!isValidEmail(emailInputValue)) {
+    addErrorMessage();
+    return;
+  }
+
+  window.location.href = "./users.html";
+});
+
+emailInput.addEventListener("focus", () => {
+  loginErrorMessage.classList.add("hide");
+  emailInput.style.border = "1px solid var(--footer-copyright)";
+});
+
+emailInput.addEventListener("blur", () => {
+  const emailInputValue = document.getElementById("email").value.trim();
+  if (!isValidEmail(emailInputValue)) {
+    addErrorMessage();
+  }
 });
